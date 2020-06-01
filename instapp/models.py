548093a6e9d7e97f7c8da040ba6b0d1520a2a8f7
Frozen Feelings <kiprutohcollo@gmail.com>
2020-06-01 13:38:41 +0300

    created profile model to contain properties of user profile

--- instapp/models.py
+++ instapp/models.py
@@ -54,3 +54,27 @@ class Comments(models.Model):
         return self.comment
 
 
+class Profile(models.Model):
+    profile_photo = models.ImageField(upload_to='images')
+    bio = HTMLField()
+    user = models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True)
+    
+    
+    def __str__(self):
+        
+        return self.profile_photo.url
+    
+    def save_profile(self):
+        self.save()
+        
+    def delete_profile(self):
+        self.delete()
+    
+    @classmethod   
+    def update_bio(cls,id,new_bio):
+        cls.objects.filter(pk = id).update(bio=new_bio)
+        new_bio_object = cls.objects.get(bio = new_bio)
+        new_bio = new_bio_object.bio
+        return new_bio        
+
+
